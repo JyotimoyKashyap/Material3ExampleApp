855b2039a0135b05735319a108717ed979e8d2c2
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2022-01-12 23:20:46 +0530

    adapter set up complete

--- app/src/main/java/com/jyotimoykashyap/demoresourcehub/MainActivity.kt
+++ app/src/main/java/com/jyotimoykashyap/demoresourcehub/MainActivity.kt
@@ -7,10 +7,34 @@ import com.jyotimoykashyap.demoresourcehub.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: CustomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val gdscList = listOf<Gdsc>(
+            Gdsc("GDSC NIT Silchar" , getString(R.string.dummy_text)),
+            Gdsc("GDSC NIT Agartala" , getString(R.string.dummy_text)),
+            Gdsc("GDSC NIT Surathkal" , getString(R.string.dummy_text)),
+            Gdsc("GDSC NIT Trichy" , getString(R.string.dummy_text)),
+            Gdsc("GDSC NIT Durgapur" , getString(R.string.dummy_text)),
+            Gdsc("GDSC Assam Engineering College" , getString(R.string.dummy_text)),
+            Gdsc("GDSC IIIT Guwahati" , getString(R.string.dummy_text)),
+            Gdsc("GDSC LPU" , getString(R.string.dummy_text)),
+            Gdsc("GDSC Sarla Birla" , getString(R.string.dummy_text)),
+            Gdsc("GDSC Women's College" , getString(R.string.dummy_text)),
+            Gdsc("GDSC Delhi" , getString(R.string.dummy_text)),
+            Gdsc("GDSC Meerut" , getString(R.string.dummy_text)),
+        )
+
+        adapter = CustomAdapter(CustomDiffUtil())
+        adapter.submitList(gdscList)
+
+        binding.run {
+            gdscListView.adapter = adapter
+        }
+
     }
 }
